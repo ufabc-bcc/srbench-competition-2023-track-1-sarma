9b172a1128a0c1eef870995ce570079593ce3a35
Aleksandar <aleksandar.kartelj@gmail.com>
2023-06-27 09:48:10 +0200

    Cleanup.

--- src/run_all.sh
+++
@@ -1,3 +0,0 @@
-python run.py "instances/srbench_2023" "dataset_1.txt" 180 20
-python run.py "instances/srbench_2023" "dataset_2.txt" 100 10
-python run.py "instances/srbench_2023" "dataset_3.txt" 180 20
